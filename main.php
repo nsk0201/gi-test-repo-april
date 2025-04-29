41c666839c2f5881429f16972b9c57c73413e0b2
Garry Rodriguez - Kredo <garry@Garrys-MacBook-Air.local>
2025-04-29 15:35:04 +0800

    added an html contents

--- main.php
+++ main.php
@@ -1,3 +1,11 @@
-<?php
-
-echo "Hello, I'm in GI Class :)";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+</head>
+<body>
+    <h1>Hello, from GI Class</h1>
+</body>
+</html>
